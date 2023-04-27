f3c94187bfd12f446139cc23d4effe8fa2f5d8da
Aryan <aryankushwahalifenote@gmail.com>
2023-04-27 17:56:40 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Article from './components/article/Article'
+import Brand from './components/brand/Brand'
+import CTA from './components/cta/CTA'
+import Feature from './components/feature/Feature'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>gpt-3</h1>
+      <Article/>
     </div>
   );
 }
---
+++ src/container/features/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './features.css'
+
+const Features = () => {
+  return (
+    <div>
+      feature
+    </div>
+  )
+}
+
+export default Features
